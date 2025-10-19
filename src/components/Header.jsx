a8a7830f6cc1c13e05eded85f6ad95c3841e2f5c
Aditta Chakraborty <cseaditta@gmail.com>
2025-10-20 04:27:20 +0600

    header design implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { format } from "date-fns";
 import logo from "../assets/logo.png"
 
 const Header = () => {
     return (
         <div className="flex flex-col justify-center items-center mt-[50px]">
             <img className="w-[471px] h-[60px]" src={logo} alt="logo" />
-            <p className="mt-5">Journalism Without Fear or Favour</p>
+            <p className="text-accent text-lg mt-5">Journalism Without Fear or Favour</p>
+            <p className="font-medium text-[20px] mt-2.5"><span className="text-primary">{format(new Date(), "EEEE")},</span> <span className="text-accent">{format(new Date(), "MMMM dd, yyyy")}</span></p>
         </div>
     );
 };
