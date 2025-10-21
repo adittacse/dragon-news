39a4dbf86cc123d1df65fcb01030bb8fe2dcc103
Aditta Chakraborty <cseaditta@gmail.com>
2025-10-22 04:48:39 +0600

    .env file created

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDTlojsGQpCtVFjhSvy6fjX_Aj34RmT0TQ",
-    authDomain: "dragon-news-ba5fc.firebaseapp.com",
-    projectId: "dragon-news-ba5fc",
-    storageBucket: "dragon-news-ba5fc.firebasestorage.app",
-    messagingSenderId: "273468325985",
-    appId: "1:273468325985:web:404bd94672a6b92d452a20",
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
